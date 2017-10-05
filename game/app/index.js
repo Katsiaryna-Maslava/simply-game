a4de6802bbdf19ce2c759b85de43e0119f5964b6
Жорж <nounga@yandex.ru>
2017-10-05 11:13:31 +0300

    Инициализация игры

---
+++ game/app/index.js
@@ -0,0 +1,6 @@
+var $canvas = document.getElementById('game');
+var api = $canvas.getContext('2d');
+api.fillStyle = 'rgb(200,0,0)';
+api.fillRect(10, 10, 55, 50);
+api.fillStyle = 'rgba(0, 0, 200, 0.5)';
+api.fillRect(30, 30, 55, 50);
